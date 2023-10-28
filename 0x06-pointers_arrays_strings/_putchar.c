8d9186c651289bcfb15701e25ec32c536fe1c1ed
SALIMCHT <saliimchatbii@gmail.com>
2023-10-28 14:17:19 -0700

    created _putchar.c

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to the standard output
+ * @c: The character to print
+ *
+ * Return: On success, 1. On error, -1.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
